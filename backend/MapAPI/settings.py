5763b37f773d62f0da44686e019a77d8a6c18811
Godley <charlotte.godley@hotmail.co.uk>
2018-03-08 23:34:07 +0000

    chore: migrate to heroku postgres

--- backend/MapAPI/settings.py
+++ backend/MapAPI/settings.py
@@ -82,6 +82,16 @@ WSGI_APPLICATION = 'MapAPI.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('PG_DB'),
+        'USER': os.environ.get('PG_USER'),
+        'PASSWORD': os.environ.get('PG_PASSWORD'),
+        'HOST': os.environ.get('PG_HOST'),
+        'PORT': os.environ.get('PG_PORT', ''),
+    }
+}
 
 DATABASES = {
     'default': {
